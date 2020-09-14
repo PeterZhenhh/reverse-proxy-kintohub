8f5a9673bc61c430a2105270b7e3bd2bbfbdcab5
wangchunin <1104436919@qq.com>
2020-09-15 04:41:43 +0800

    change to local config

--- proxy.sh
+++ proxy.sh
@@ -2,6 +2,6 @@
 
 # start tor v2ray
 
-caddy run --config $CONFIG --adapter caddyfile
-#caddy run --config ./config.json --adapter caddyfile
+#caddy run --config $CONFIG --adapter caddyfile
+caddy run --config /config.json --adapter caddyfile
 #caddy reverse-proxy  -to https://ishare.melulu.workers.dev --from :3000  --change-host-header
