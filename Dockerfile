918e78d2dfabf4aa8aca085cd43ba14c1fb6f8dd
wangchunin <1104436919@qq.com>
2020-09-15 05:39:04 +0800

    last test

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,4 @@
 FROM caddy:2.1.1-alpine
 ADD proxy.sh /proxy.sh
-ADD config.json /config.json
 RUN chmod +x /proxy.sh
-#ENV CONFIG="https://raw.githubusercontent.com/wangchunin/reverse-proxy-kintohub/master/config.json"
 CMD /proxy.sh
--- proxy.sh
+++ proxy.sh
@@ -1,7 +1,2 @@
 #!/bin/sh
-
-# start tor v2ray
-
-#caddy run --config $CONFIG --adapter caddyfile
-#caddy run --config /config.json --adapter caddyfile
-caddy reverse-proxy  -to ishare.melulu.workers.dev --from :3000  --change-host-header
+caddy reverse-proxy  -to $HOST --from :3000  --change-host-header
