dabf9ebf99f2a0dd6c56cbd4642b38abb71bf235
wangchunin <1104436919@qq.com>
2020-09-15 04:18:57 +0800

    first

---
+++ proxy.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# start tor v2ray
+
+caddy run --config $CONFIG --adapter caddyfile
+#caddy run --config ./config.json --adapter caddyfile
+#caddy reverse-proxy  -to https://ishare.melulu.workers.dev --from :3000  --change-host-header
