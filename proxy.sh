c05fc30f0e7554585a969aac621bd633ca82bff2
wangchunin <1104436919@qq.com>
2020-09-15 04:45:58 +0800

    change to cmd

--- proxy.sh
+++ proxy.sh
@@ -3,5 +3,5 @@
 # start tor v2ray
 
 #caddy run --config $CONFIG --adapter caddyfile
-caddy run --config /config.json --adapter caddyfile
-#caddy reverse-proxy  -to https://ishare.melulu.workers.dev --from :3000  --change-host-header
+#caddy run --config /config.json --adapter caddyfile
+caddy reverse-proxy  -to ishare.melulu.workers.dev --from :3000  --change-host-header
